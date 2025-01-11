e31488205840545f090a888710cec2284cf1c60c
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-01-11 15:44:30 +0100

    Added DbContext (with logging configured), connection string for SQL Server (in appsettings file), and JsonSerializerOptions reference handler to prevent circular references.

---
+++ Lab2_LibraryWebAPI/Data/BooksDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lab2_LibraryWebAPI.Data
+{
+    public class BooksDbContext : DbContext
+    {
+        public BooksDbContext(DbContextOptions<BooksDbContext> options) : base(options)
+        {
+        }
+    }
+}
--- Lab2_LibraryWebAPI/Program.cs
+++ Lab2_LibraryWebAPI/Program.cs
@@ -1,4 +1,9 @@
 
+using System.Diagnostics;
+using System.Text.Json.Serialization;
+using Lab2_LibraryWebAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace Lab2_LibraryWebAPI
 {
     public class Program
@@ -9,12 +14,28 @@ namespace Lab2_LibraryWebAPI
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers().AddJsonOptions(options =>
+                options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+            builder.Services.AddDbContext<BooksDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("BooksDb"))
+                    .LogTo(message => Debug.WriteLine(message))
+                    .EnableSensitiveDataLogging());
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
+
+            /*
+            using (var scope = app.Services.CreateScope())
+               {
+                   var db = scope.ServiceProvider.GetRequiredService<BooksDbContext>();
+                   db.Database.EnsureDeleted();
+                   db.Database.EnsureCreated();
+               }
+            */
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
