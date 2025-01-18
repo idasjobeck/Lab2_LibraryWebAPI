60fc8fc8f70d069eb7015606edd51c9177b406f3
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-01-18 18:25:27 +0100

    Added Loan DTOs.

---
+++ Lab2_LibraryWebAPI/DTOs/CreateLoanDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Lab2_LibraryWebAPI.DTOs
+{
+    public class CreateLoanDTO
+    {
+        public int BookId { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ Lab2_LibraryWebAPI/DTOs/ReturnLoanDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace Lab2_LibraryWebAPI.DTOs
+{
+    public class ReturnLoanDTO
+    {
+        public int? BookRating { get; set; }
+    }
+}
