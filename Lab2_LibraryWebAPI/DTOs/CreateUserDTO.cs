d04aeecfee20c4aeb320db35346be6647ad1fcd2
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-01-18 18:24:03 +0100

    Added User DTOs.

---
+++ Lab2_LibraryWebAPI/DTOs/CreateUserDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Lab2_LibraryWebAPI.DTOs
+{
+    public class CreateUserDTO
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public string? Email { get; set; }
+    }
+}
---
+++ Lab2_LibraryWebAPI/DTOs/DisplayUserDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab2_LibraryWebAPI.DTOs
+{
+    public class DisplayUserDTO
+    {
+        public int CardNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? Email { get; set; }
+    }
+}
