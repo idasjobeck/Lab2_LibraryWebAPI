d4bd89ed2964ef4e9016c384b3cf3b5d309c9c91
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-01-18 17:46:53 +0100

    Added Author DTOs.

---
+++ Lab2_LibraryWebAPI/DTOs/AuthorNameDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Lab2_LibraryWebAPI.DTOs
+{
+    public class AuthorNameDTO
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+    }
+}
---
+++ Lab2_LibraryWebAPI/DTOs/AuthorNameWithIdDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Lab2_LibraryWebAPI.DTOs
+{
+    public class AuthorNameWithIdDTO
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
