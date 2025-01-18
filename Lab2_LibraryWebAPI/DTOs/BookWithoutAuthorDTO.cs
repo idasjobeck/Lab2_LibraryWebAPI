2d4379e545e0835ed15a7be2087b029bec062400
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-01-18 18:26:38 +0100

    Added BookWithoutAuthorDTO.

---
+++ Lab2_LibraryWebAPI/DTOs/BookWithoutAuthorDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace Lab2_LibraryWebAPI.DTOs
+{
+    public class BookWithoutAuthorDTO
+    {
+        public required string Title { get; set; }
+        public string? Series { get; set; }
+        public int? NumberInSeries { get; set; }
+        public string Genre { get; set; }
+        public string ISBN { get; set; }
+        public int PublishedYear { get; set; }
+        public string Publisher { get; set; }
+        public string Edition { get; set; }
+        public int TotalQty { get; set; }
+        public int AvailableQty { get; set; }
+    }
+}
