355ce75514bd2f613ac8f445a5adc1bbae51664b
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-01-18 17:21:10 +0100

    Refactored AuthorId to Id for Author entity and created migration for the update.

--- Lab2_LibraryWebAPI/Migrations/20250118161829_RefactoredAuthorId.Designer.cs
+++ Lab2_LibraryWebAPI/Migrations/20250118161829_RefactoredAuthorId.Designer.cs
@@ -4,6 +4,7 @@ using Lab2_LibraryWebAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Lab2_LibraryWebAPI.Migrations
 {
     [DbContext(typeof(BooksDbContext))]
-    partial class BooksDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250118161829_RefactoredAuthorId")]
+    partial class RefactoredAuthorId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Lab2_LibraryWebAPI/Migrations/20250118161829_RefactoredAuthorId.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lab2_LibraryWebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefactoredAuthorId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AuthorBook_Authors_AuthorsAuthorId",
+                table: "AuthorBook");
+
+            migrationBuilder.RenameColumn(
+                name: "AuthorId",
+                table: "Authors",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "AuthorsAuthorId",
+                table: "AuthorBook",
+                newName: "AuthorsId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AuthorBook_Authors_AuthorsId",
+                table: "AuthorBook",
+                column: "AuthorsId",
+                principalTable: "Authors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AuthorBook_Authors_AuthorsId",
+                table: "AuthorBook");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Authors",
+                newName: "AuthorId");
+
+            migrationBuilder.RenameColumn(
+                name: "AuthorsId",
+                table: "AuthorBook",
+                newName: "AuthorsAuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AuthorBook_Authors_AuthorsAuthorId",
+                table: "AuthorBook",
+                column: "AuthorsAuthorId",
+                principalTable: "Authors",
+                principalColumn: "AuthorId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
