bd45b7e1390abf850b4af8956149bd9ea4d61d07
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-01-18 17:32:39 +0100

    Migration having updated and configured Loan and Book entities to SetNull, and having added entity and config for AuthorBook junction table.

---
+++ Lab2_LibraryWebAPI/Migrations/20250118163142_ModelRevisionChangingCascadeDeleteAndAddingAuthorBookJunctionEntity.cs
@@ -0,0 +1,180 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lab2_LibraryWebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModelRevisionChangingCascadeDeleteAndAddingAuthorBookJunctionEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AuthorBook_Authors_AuthorsId",
+                table: "AuthorBook");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AuthorBook_Books_BooksId",
+                table: "AuthorBook");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Loans_Books_BookId",
+                table: "Loans");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Loans_Users_UserId",
+                table: "Loans");
+
+            migrationBuilder.RenameColumn(
+                name: "BooksId",
+                table: "AuthorBook",
+                newName: "BookId");
+
+            migrationBuilder.RenameColumn(
+                name: "AuthorsId",
+                table: "AuthorBook",
+                newName: "AuthorId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AuthorBook_BooksId",
+                table: "AuthorBook",
+                newName: "IX_AuthorBook_BookId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Loans",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BookId",
+                table: "Loans",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AuthorBook_Authors_AuthorId",
+                table: "AuthorBook",
+                column: "AuthorId",
+                principalTable: "Authors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AuthorBook_Books_BookId",
+                table: "AuthorBook",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Loans_Books_BookId",
+                table: "Loans",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Loans_Users_UserId",
+                table: "Loans",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AuthorBook_Authors_AuthorId",
+                table: "AuthorBook");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AuthorBook_Books_BookId",
+                table: "AuthorBook");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Loans_Books_BookId",
+                table: "Loans");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Loans_Users_UserId",
+                table: "Loans");
+
+            migrationBuilder.RenameColumn(
+                name: "BookId",
+                table: "AuthorBook",
+                newName: "BooksId");
+
+            migrationBuilder.RenameColumn(
+                name: "AuthorId",
+                table: "AuthorBook",
+                newName: "AuthorsId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AuthorBook_BookId",
+                table: "AuthorBook",
+                newName: "IX_AuthorBook_BooksId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Loans",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BookId",
+                table: "Loans",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AuthorBook_Authors_AuthorsId",
+                table: "AuthorBook",
+                column: "AuthorsId",
+                principalTable: "Authors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AuthorBook_Books_BooksId",
+                table: "AuthorBook",
+                column: "BooksId",
+                principalTable: "Books",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Loans_Books_BookId",
+                table: "Loans",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Loans_Users_UserId",
+                table: "Loans",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
