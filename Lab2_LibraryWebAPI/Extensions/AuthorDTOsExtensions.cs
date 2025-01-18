fc76720715e4e51d212ae61cb2f455d9f6d48e0f
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-01-18 18:33:53 +0100

    Added Author DTOs extension methods.

---
+++ Lab2_LibraryWebAPI/Extensions/AuthorDTOsExtensions.cs
@@ -0,0 +1,51 @@
+﻿using Lab2_LibraryWebAPI.DTOs;
+using Lab2_LibraryWebAPI.Models;
+
+namespace Lab2_LibraryWebAPI.Extensions
+{
+    public static class AuthorDTOsExtensions
+    {
+        public static Author ToAuthor(this AuthorNameDTO authorNameDto)
+        {
+            return new Author
+            {
+                FirstName = authorNameDto.FirstName,
+                LastName = authorNameDto.LastName
+            };
+        }
+
+        public static List<Author> ToAuthors(this List<AuthorNameDTO> authorNameDtos)
+        {
+            return authorNameDtos.Select(a => a.ToAuthor()).ToList();
+        }
+
+        public static AuthorNameDTO ToAuthorNameDTO(this Author author)
+        {
+            return new AuthorNameDTO
+            {
+                FirstName = author.FirstName,
+                LastName = author.LastName
+            };
+        }
+
+        public static List<AuthorNameDTO> ToAuthorNameDTOs(this List<Author> authors)
+        {
+            return authors.Select(a => a.ToAuthorNameDTO()).ToList();
+        }
+
+        public static AuthorNameWithIdDTO ToAuthorNameWithIdDTO(this Author author)
+        {
+            return new AuthorNameWithIdDTO()
+            {
+                Id = author.Id,
+                FirstName = author.FirstName,
+                LastName = author.LastName
+            };
+        }
+
+        public static List<AuthorNameWithIdDTO> ToAuthorNameWithIdDTOs(this List<Author> authors)
+        {
+            return authors.Select(a => a.ToAuthorNameWithIdDTO()).ToList();
+        }
+    }
+}
