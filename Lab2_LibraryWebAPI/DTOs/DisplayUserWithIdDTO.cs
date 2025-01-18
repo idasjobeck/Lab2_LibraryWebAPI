bd55a84c86272e9a8b4343fb80546c4df591ff46
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-01-18 19:19:41 +0100

    Added DisplayUserWithIdDTO and User DTOs extension methods.

---
+++ Lab2_LibraryWebAPI/DTOs/DisplayUserWithIdDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab2_LibraryWebAPI.DTOs
+{
+    public class DisplayUserWithIdDTO
+    {
+        public int Id { get; set; }
+        public int CardNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? Email { get; set; }
+    }
+}
---
+++ Lab2_LibraryWebAPI/Extensions/UserDTOsExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Lab2_LibraryWebAPI.DTOs;
+using Lab2_LibraryWebAPI.Models;
+
+namespace Lab2_LibraryWebAPI.Extensions
+{
+    public static class UserDTOsExtensions
+    {
+        public static DisplayUserDTO ToDisplayUserDTO(this User user) =>
+            new DisplayUserDTO
+            {
+                CardNumber = user.CardNumber,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email
+            };
+
+        public static DisplayUserWithIdDTO ToDisplayUserWithIdDTO(this User user) =>
+            new DisplayUserWithIdDTO
+            {
+                Id = user.Id,
+                CardNumber = user.CardNumber,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email
+            };
+    }
+}
