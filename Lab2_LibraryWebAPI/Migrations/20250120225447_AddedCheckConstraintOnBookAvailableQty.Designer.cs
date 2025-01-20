7d400ec6a92eeeb550e55f568cb7c8d5019648f7
Ida Sjöbeck <ida.sjobeck@gmail.com>
2025-01-20 23:59:13 +0100

    Migration having added Check Constraint to Book AvailableQty.

--- Lab2_LibraryWebAPI/Migrations/20250120225447_AddedCheckConstraintOnBookAvailableQty.Designer.cs
+++ Lab2_LibraryWebAPI/Migrations/20250120225447_AddedCheckConstraintOnBookAvailableQty.Designer.cs
@@ -4,6 +4,7 @@ using Lab2_LibraryWebAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Lab2_LibraryWebAPI.Migrations
 {
     [DbContext(typeof(BooksDbContext))]
-    partial class BooksDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250120225447_AddedCheckConstraintOnBookAvailableQty")]
+    partial class AddedCheckConstraintOnBookAvailableQty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Lab2_LibraryWebAPI/Migrations/20250120225447_AddedCheckConstraintOnBookAvailableQty.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lab2_LibraryWebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCheckConstraintOnBookAvailableQty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Book_AvailableQty",
+                table: "Books",
+                sql: "AvailableQty <= TotalQty");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Book_AvailableQty",
+                table: "Books");
+        }
+    }
+}
